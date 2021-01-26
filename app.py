63e5a7aa4d0bade7e46bb07c10a8706d7e8c6133
augusto <augustoantonelli88@gmail.com>
2021-01-26 02:26:44 -0300

    little changes

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 
 app =  Flask(__name__)
@@ -11,14 +11,46 @@ def landing():
 
 @app.route('/logIn')
 def logIn():
-   return ('logIn')
+   logIn="active"
+   return render_template("logIn.html", logIn=logIn)
 
-@app.route('/SigIn')
+
+@app.route('/SigIn', methods=["GET", "POST"])
 def SigIn():
-   return ('SigIn')
 
-@app.route('/program')
+   if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email']
+        password = request.form['password']
+
+        print(name, email, password)
+
+        next = request.args.get('next', None)
+        if next:
+            return redirect(next)
+        return redirect(url_for('landing'))
+
+   SigIn="active"
+   return render_template("SigIn.html", SigIn=SigIn)
+
+
+@app.route('/program', methods=["GET", "POST"])
 def program():
+
+   if request.method == 'POST':
+        name = request.form['name']
+        massage = request.form['massage']
+        start_time = request.form['start_time']
+        end_time = request.form['end_time']
+        _filter = request.form['_filter']
+
+        print(name, massage, start_time, end_time, _filter)
+
+        next = request.args.get('next', None)
+        if next:
+            return redirect(next)
+        return redirect(url_for('landing'))
+
    program = "active"
    return render_template('program.html', program=program)
 
